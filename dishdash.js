7bb4c3b34011d4e7937722cabcdc8da029aba784
kiffie.coetzee <kif@idealweb.co.za>
2024-04-24 10:14:32 +0200

    script

--- dishdash.js
+++ dishdash.js
@@ -1,12 +1,14 @@
 const link = document.createElement("link");
 link.rel = "stylesheet";
-link.href = "path/to/your/index.css";
+link.href = "https://kiffiecoetzee.github.io/index.css";
 document.head.appendChild(link);
 
 // Load external JavaScript file from CDN
 const script = document.createElement("script");
-script.src = "https://cdn.example.com/path/to/external.js";
+script.src = "https://kiffiecoetzee.github.io/index.js";
 script.onload = function () {
     // Code to execute after external script is loaded, if needed
 };
 document.body.appendChild(script);
+
+// git push -u origin main
